b9cc87464c4bec06a56275cc8d0f83a22e821f03
Brandon Tweed <zbrandontweed@gmail.com>
2020-07-08 15:44:30 -0500

    Add some clarification markers when printing out headers and data

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/api/policies/v1.0/policies')
-def hello():
+def print_request_headers(request):
+    print("Request Headers:")
+    print("BEGIN")
     print(request.headers)
+    print("END")
+
+
+def print_request_body(request):
+    print("Request Data:")
+    print("BEGIN")
+    print(request.get_data(as_text=True))
+    print("END")
+
+
+@app.route('/api/policies/v1.0/policies')
+def policies():
+    print_request_headers(request)
+    print_request_body(request)
     return "Hello World!"
 
 if __name__ == '__main__':
